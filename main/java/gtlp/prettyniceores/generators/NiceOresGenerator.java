a289fa86f1c60173174a47757c92013a28e41ecf
GiantTree <marv.1997@live.de>
2016-05-23 23:48:28 +0200

    Optimize world gen. Added parallel streams. More optimization should be possible.

--- main/java/gtlp/prettyniceores/generators/NiceOresGenerator.java
+++ main/java/gtlp/prettyniceores/generators/NiceOresGenerator.java
@@ -2,10 +2,10 @@ package gtlp.prettyniceores.generators;
 
 import gtlp.prettyniceores.PrettyNiceOres;
 import gtlp.prettyniceores.interfaces.IOreDictCompatible;
-import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenerator;
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 import net.minecraftforge.oredict.OreDictionary;
 
 import java.util.Random;
+import java.util.stream.IntStream;
 
 /**
  * Created by Marv1 on 23.05.2016 as part of forge-modding-1.9.
@@ -24,26 +25,25 @@ public class NiceOresGenerator implements IWorldGenerator {
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+        Chunk chunk = chunkProvider.getLoadedChunk(chunkX, chunkZ);
         switch (world.provider.getDimension()) {
             case 0:
-                for (int z = chunkZ * CHUNKSIZE; z < chunkZ * CHUNKSIZE + CHUNKSIZE; z++) {
-                    for (int y = 0; y < MAX_HEIGHT; y++) {
-                        for (int x = chunkX * CHUNKSIZE; x < chunkX * CHUNKSIZE + CHUNKSIZE; x++) {
-                            BlockPos blockPos = new BlockPos(x, y, z);
-                            PrettyNiceOres.blockList.forEach((name, block) -> {
-                                if (block instanceof IOreDictCompatible) {
-                                    Block blockIn = world.getBlockState(blockPos).getBlock();
-                                    OreDictionary.getOres(((IOreDictCompatible) block).getOreDictType()).forEach(itemStack -> {
-                                        if (ItemStack.areItemStacksEqual(itemStack, new ItemStack(blockIn))) {
-                                            world.setBlockState(blockPos, block.getDefaultState());
-                                        }
-                                    });
-                                }
-                            });
-
-                        }
+                PrettyNiceOres.blockList.entrySet().parallelStream().forEach(entry -> {
+                    if (entry.getValue() instanceof IOreDictCompatible) {
+                        OreDictionary.getOres(((IOreDictCompatible) entry.getValue()).getOreDictType()).forEach(itemStack -> {
+                            IntStream.rangeClosed(0, world.getActualHeight()).parallel().forEach(y ->
+                                    IntStream.rangeClosed(0, CHUNKSIZE).parallel().forEach(z ->
+                                            IntStream.rangeClosed(0, CHUNKSIZE).parallel().forEach(x -> {
+                                                BlockPos blockPos = new BlockPos(x, y, z);
+                                                if ((itemStack.isItemEqual(new ItemStack(chunk.getBlockState(blockPos).getBlock())))) {
+                                                    synchronized (this) {
+                                                        chunk.setBlockState(blockPos, entry.getValue().getDefaultState());
+                                                    }
+                                                }
+                                            })));
+                        });
                     }
-                }
+                });
                 break;
         }
     }
