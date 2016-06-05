d66f8d00640094d6a31d4ec66111b0f18eebfd39
GiantTree <marv.1997@live.de>
2016-06-05 21:55:39 +0200

    Bump version number for PR

--- src/main/java/gtlp/prettyniceores/PrettyNiceOres.java
+++ src/main/java/gtlp/prettyniceores/PrettyNiceOres.java
@@ -38,7 +38,7 @@ import static net.minecraftforge.oredict.RecipeSorter.Category.SHAPELESS;
 @Mod(modid = PrettyNiceOres.MOD_ID, version = PrettyNiceOres.VERSION, canBeDeactivated = true, dependencies = "after:neotech@[1.9-3.0.6,);after:tconstruct@[1.9.4-2.3.1,)", acceptedMinecraftVersions = "[1.9.4,)")
 public class PrettyNiceOres {
     public static final String MOD_ID = "prettyniceores";
-    public static final String VERSION = "1.9.4-0.3.3";
+    public static final String VERSION = "1.9.4-0.3.4";
     public static final Map<String, Block> blockList = new HashMap<>();
     public static final Map<String, Item> itemList = new HashMap<>();
     public static final Map<String, ItemBlock> itemBlockList = new HashMap<>();
