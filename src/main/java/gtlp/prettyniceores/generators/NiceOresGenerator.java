7bbd3207aae48a12ee384e58a5c5e89cd18e45b3
GiantTree <marv.1997@live.de>
2016-07-03 15:17:59 +0200

    Rewrite the generator, now checks each block just once and also keeps a replacement list for quick chunk generation.

--- src/main/java/gtlp/prettyniceores/generators/NiceOresGenerator.java
+++ src/main/java/gtlp/prettyniceores/generators/NiceOresGenerator.java
@@ -1,9 +1,11 @@
 package gtlp.prettyniceores.generators;
 
 import gtlp.prettyniceores.PrettyNiceOres;
+import gtlp.prettyniceores.interfaces.IOre;
 import gtlp.prettyniceores.interfaces.IOreDictCompatible;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkGenerator;
@@ -12,6 +14,7 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 import net.minecraftforge.oredict.OreDictionary;
 
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -22,26 +25,32 @@ public class NiceOresGenerator implements IWorldGenerator {
 
     //Size of storage array (cube with indices 0 to 15, ie. 16)
     private static final int STORAGE_ARRAY_SIZE = 16;
+    private final ConcurrentHashMap<Block, Block> replacementMap = new ConcurrentHashMap<>();
+
+    public NiceOresGenerator() {
+        PrettyNiceOres.blockList.entrySet().stream().filter(entry -> entry.getValue() instanceof IOre && entry.getValue() instanceof IOreDictCompatible).forEach(niceOre -> {
+            OreDictionary.getOres(((IOreDictCompatible) niceOre.getValue()).getOreDictType()).forEach(stack -> {
+                if (stack.getItem() instanceof ItemBlock) {
+                    replacementMap.put(((ItemBlock) stack.getItem()).block, niceOre.getValue());
+                }
+            });
+        });
+    }
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
         Chunk chunk = chunkProvider.provideChunk(chunkX, chunkZ);
         //Fairly quick nested loop to replace vanilla and ore dictionary ores with ours upon generation of a chunk.
-        PrettyNiceOres.blockList.entrySet().parallelStream().filter(entry -> entry.getValue() instanceof IOreDictCompatible).forEach(entry ->
-                OreDictionary.getOres(((IOreDictCompatible) entry.getValue()).getOreDictType()).stream().filter(itemStack -> !itemStack.isItemEqual(new ItemStack(entry.getValue()))).forEach(itemStack -> {
-                    Stream.of(chunk.getBlockStorageArray()).filter(blockStorage -> blockStorage != null).forEach(blockStorage ->
-                            IntStream.range(0, STORAGE_ARRAY_SIZE).forEach(y ->
-                                    IntStream.range(0, STORAGE_ARRAY_SIZE).forEach(z ->
-                                            IntStream.range(0, STORAGE_ARRAY_SIZE).forEach(x -> {
-                                                        IBlockState state = blockStorage.get(x, y, z);
-                                                        if (state != null && itemStack.isItemEqual(new ItemStack(state.getBlock()))) {
-                                                            synchronized (this) {
-                                                                blockStorage.getData().set(x, y, z, entry.getValue().getDefaultState());
-                                                            }
-                                                        }
-                                                    }
-
-                                            ))));
-                }));
+        Stream.of(chunk.getBlockStorageArray()).filter(blockStorage -> blockStorage != null).parallel().forEach(blockStorage ->
+                IntStream.range(0, STORAGE_ARRAY_SIZE).forEach(y ->
+                        IntStream.range(0, STORAGE_ARRAY_SIZE).forEach(z ->
+                                IntStream.range(0, STORAGE_ARRAY_SIZE).forEach(x -> {
+                                    IBlockState state = blockStorage.get(x, y, z);
+                                    if (state != null && replacementMap.containsKey(state.getBlock())) {
+                                        synchronized (this) {
+                                            blockStorage.getData().set(x, y, z, replacementMap.get(state.getBlock()).getDefaultState());
+                                        }
+                                    }
+                                }))));
     }
 }
