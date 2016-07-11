f5c23ddcebed06a0e9bc2f5980f7faeda1ee3e28
GiantTree <webmaster@gianttree.de>
2016-07-11 15:50:27 +0200

    Make sure the mod loads after Substratum

--- src/main/java/gtlp/prettyniceores/Constants.java
+++ src/main/java/gtlp/prettyniceores/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     public static final String VERSION = "${version}";
     public static final String UPDATE_URL = "https://raw.githubusercontent.com/GiantTreeLP/PrettyNiceOres/1.9.4/versions.json";
     public static final String NAME = "PrettyNiceOres";
-    public static final String DEPENDENCIES = "after:neotech;after:tconstruct;after:basemetals";
+    public static final String DEPENDENCIES = "after:neotech;after:tconstruct;after:basemetals;after:substratum";
     protected static final String MC_VERSION = "[${mcversion},)";
 
     private Constants() {
