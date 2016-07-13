e956c7262963d13cf458aef5ed1640b602e4b91d
GiantTree <webmaster@gianttree.de>
2016-07-13 17:11:04 +0200

    Backport all changes made in the 1.10.2 branch

--- src/main/java/gtlp/prettyniceores/Constants.java
+++ src/main/java/gtlp/prettyniceores/Constants.java
@@ -6,11 +6,11 @@ package gtlp.prettyniceores;
 public final class Constants {
 
     public static final String MOD_ID = "prettyniceores";
-    public static final String VERSION = "${version}";
-    public static final String UPDATE_URL = "https://raw.githubusercontent.com/GiantTreeLP/PrettyNiceOres/1.9.4/versions.json";
     public static final String NAME = "PrettyNiceOres";
-    public static final String DEPENDENCIES = "after:neotech;after:tconstruct;after:basemetals";
-    protected static final String MC_VERSION = "[${mcversion},)";
+    static final String VERSION = "${version}";
+    static final String UPDATE_URL = "https://raw.githubusercontent.com/GiantTreeLP/PrettyNiceOres/1.9.4/versions.json";
+    static final String DEPENDENCIES = "after:neotech;after:tconstruct;after:basemetals;after:substratum";
+    static final String MC_VERSION = "[${mcversion},)";
 
     private Constants() {
     }
--- src/main/java/gtlp/prettyniceores/interfaces/ISmeltable.java
+++ src/main/java/gtlp/prettyniceores/interfaces/ISmeltable.java
@@ -2,6 +2,8 @@ package gtlp.prettyniceores.interfaces;
 
 import net.minecraft.item.ItemStack;
 
+import javax.annotation.Nullable;
+
 /**
  * Created by Marv1 on 23.05.2016 as part of forge-modding-1.9.
  */
@@ -9,6 +11,7 @@ public interface ISmeltable {
     /**
      * @return an {@link net.minecraft.item.ItemStack) containing the result of the smelting procedure
      */
+    @Nullable
     ItemStack getSmeltingResult();
 
     /**
