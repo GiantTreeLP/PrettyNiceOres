801e6df494807a0eb0a9ea4987ff593a22cabd8d
GiantTree <webmaster@gianttree.de>
2016-07-25 16:09:05 +0200

    Remove redudant field.

--- src/main/java/gtlp/prettyniceores/config/NiceConfig.java
+++ src/main/java/gtlp/prettyniceores/config/NiceConfig.java
@@ -8,7 +8,6 @@ import java.io.File;
  * Created by Marv1 on 22.07.2016.
  */
 public final class NiceConfig extends Configuration {
-    private Configuration config;
 
     public NiceConfig(File configFile) {
         super(configFile);
